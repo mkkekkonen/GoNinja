f85bb219e35b9c85e9b85b19b76dffe44156efa6
mkkekkonen2 <mkkekkonen@protonmail.com>
2023-06-11 22:29:43 +0300

    Ninja attack implementation

--- scripts/NinjaPhysics.cs
+++ scripts/NinjaPhysics.cs
@@ -7,8 +7,25 @@ public partial class NinjaPhysics : CharacterBody2D
   public const float JumpVelocity = -450.0f;
   public const float Epsilon = 0.1f;
 
+  public bool Attacking { get; set; } = false;
+
   // Get the gravity from the project settings to be synced with RigidBody nodes.
   public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+  public override void _Input(InputEvent @event)
+  {
+    base._Input(@event);
+
+    var animationPlayer = GetNode<AnimationPlayer>("../AnimationPlayer");
+
+    var text = @event.AsText();
+
+    if (text == "Ctrl" && !Attacking)
+    {
+      animationPlayer.Play("attack");
+      Attacking = true;
+    }
+  }
 
   public override void _Process(double delta)
   {
@@ -28,7 +45,11 @@ public partial class NinjaPhysics : CharacterBody2D
     var animationPlayer = GetNode<AnimationPlayer>("../AnimationPlayer");
     var velocityAbs = Math.Abs(Velocity.X);
 
-    if (!IsOnFloor())
+    if (Attacking)
+    {
+      // no-op
+    }
+    else if (!IsOnFloor())
     {
       animationPlayer.Play("jump");
     }
@@ -44,20 +65,21 @@ public partial class NinjaPhysics : CharacterBody2D
 
   public override void _PhysicsProcess(double delta)
   {
-    Vector2 velocity = Velocity;
+    var velocity = Velocity;
+    var isOnFloor = IsOnFloor();
 
     // Add the gravity.
     if (!IsOnFloor())
       velocity.Y += gravity * (float)delta;
 
     // Handle Jump.
-    if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+    if (Input.IsActionJustPressed("ui_accept") && isOnFloor)
       velocity.Y = JumpVelocity;
 
     // Get the input direction and handle the movement/deceleration.
     // As good practice, you should replace UI actions with custom gameplay actions.
     Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-    if (direction != Vector2.Zero)
+    if (!(isOnFloor && Attacking) && direction != Vector2.Zero)
     {
       velocity.X = direction.X * Speed;
     }
@@ -68,5 +90,10 @@ public partial class NinjaPhysics : CharacterBody2D
 
     Velocity = velocity;
     MoveAndSlide();
+  }
+
+  public void EndAttack()
+  {
+    Attacking = false;
   }
 }
